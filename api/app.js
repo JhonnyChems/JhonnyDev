9d21b77657bea7e46a07de6d420847cb9ad48293
JhonnyChems <crewpeligrososkal@gmail.com>
2025-01-05 21:56:20 -0500

    Se agrega proyecto

---
+++ api/app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// Configuración de CORS con origen específico
+const corsOptions = {
+    origin: 'https://sitio-web-1uos-r641qjju3-jhonnychems-projects.vercel.app',
+    methods: 'GET,POST,OPTIONS',
+    allowedHeaders: 'Content-Type',
+};
+
+app.use(cors(corsOptions)); 
+app.use(express.json());  
+
+app.options('/send-email', cors(corsOptions));  
+
+app.post('/send-email', async (req, res) => {
+    const { name, email, subject, affair, phone } = req.body;
+
+    try {
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
+            },
+        });
+
+        await transporter.sendMail({
+            from: process.env.EMAIL_USER,
+            to: process.env.EMAIL_RECEP,
+            subject: subject,
+            text: `Nombre: ${name}\nTeléfono: ${phone}\nEmail: ${email}\nAsunto: ${affair}`,
+        });
+
+        res.status(200).json({ message: 'Email sent successfully!' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to send email' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ frontend/script.js
@@ -0,0 +1,44 @@
+const backendUrl = 'https://sitio-axrjgzprg-jhonnychems-projects.vercel.app'
+
+    const menuIcon = document.querySelector('#menu-icon');
+    const navbar = document.querySelector('#navbar');
+
+    menuIcon.onclick = () =>{
+        menuIcon.classList.toggle('bx-x')
+        navbar.classList.toggle('active')
+    }
+
+    //Formulario de informacion//*
+    
+
+
+    document.getElementById('contactForm').addEventListener('submit', async (event) => {
+        event.preventDefault(); 
+        const params = {
+            name: document.getElementById("name").value,
+            email: document.getElementById("email").value,
+            phone: document.getElementById("phone").value,
+            subject: document.getElementById("subject").value,
+            affair: document.getElementById("affair").value,
+        };
+
+        console.log('datos que se envian al backend', params)
+    
+        try {
+            const response = await fetch(`${backendUrl}/send-email`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(params),
+            });
+
+            console.log('respuesta del servidor', response)
+            await response.json();
+            alert("El correo se ha enviado de manera exitosa");
+        } catch (error) {
+            console.error("Error al enviar el correo:", error);
+            alert("Error al enviar el correo. Por favor, inténtalo de nuevo.");
+        }
+    });
+    
